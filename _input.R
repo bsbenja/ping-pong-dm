f480b6af353e78ba619e387992f4aa724cdfb049
bsbenja <bsbenja@gmail.com>
2026-05-10 12:47:12 +0200

    main

--- _input.R
+++ _input.R
@@ -9,10 +9,10 @@ InputEventAarAkt_V <- "Ping Pong DM 2026" # Event og år
 InputData_V        <- "filer/data/input/ping-pong-dm-tilmelding.xlsx" # Sti til Excel-fil
 
 # Kildefiler
-source(file = "filer/kode/r/_variables.R")
-source(file = "filer/kode/r/0-setup.R")
-source(file = "filer/kode/r/1-funktioner.R")
-source(file = "filer/kode/r/2-indlaesning-af-tabeller.R")
-source(file = "filer/kode/r/3-tabel-1-og-2.R")
-source(file = "filer/kode/r/4-tabel-3.R")
-source(file = "filer/kode/r/5-aktuel-input.R")
+source(file = "filer/kode/R/_variables.R")
+source(file = "filer/kode/R/0-setup.R")
+source(file = "filer/kode/R/1-funktioner.R")
+source(file = "filer/kode/R/2-indlaesning-af-tabeller.R")
+source(file = "filer/kode/R/3-tabel-1-og-2.R")
+source(file = "filer/kode/R/4-tabel-3.R")
+source(file = "filer/kode/R/5-aktuel-input.R")
